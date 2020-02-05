6cf7d017c7331320de477a8cd4d3488f36f4a4c1
Devin Beichner <devbeichner@gmail.com>
2020-02-05 09:45:42 -0500

    Some changes were made I guess?

--- src/main/java/GreatSword.java
+++ src/main/java/GreatSword.java
@@ -3,20 +3,16 @@ import java.util.Map;
 
 public class GreatSword extends Blademaster {
 
-    private String weaponType;
+    private final String WEAPON_TYPE = "severing";
 
     Map<String, Double> motionValues = new HashMap<String, Double>();
-
-    public GreatSword(String weaponName, int rarity, int attack, int affinity, int elderseal, String sharpnessColor, String element){
-        super(weaponName, rarity, attack, affinity, elderseal, sharpnessColor, element);
-        this.weaponType = "severing";
-        motionValues.put("Verticle Slash", 0.48);
-        motionValues.put("Rising Slash", 0.38);
-        motionValues.put("Cleave", 0.26);
-        motionValues.put("Tackle", 0.26);
-        motionValues.put("Charge II Tackle", 0.35);
-        motionValues.put("Charge III Tackle", 0.48);
-        motionValues.put("Diving Cleave", 0.71);
+    {motionValues.put("Verticle Slash", 0.48);
+     motionValues.put("Rising Slash", 0.38);
+     motionValues.put("Cleave", 0.26);
+     motionValues.put("Tackle", 0.26);
+     motionValues.put("Charge II Tackle", 0.35);
+     motionValues.put("Charge III Tackle", 0.48);
+     motionValues.put("Diving Cleave", 0.71);
         motionValues.put("Charge II Diving Cleave", 0.85);
         motionValues.put("Charge III Diving Cleave", 1.1);
         motionValues.put("Side Strike", 0.15);
@@ -37,7 +33,7 @@ public class GreatSword extends Blademaster {
         motionValues.put("True Charge Slash II (first hit)", 0.20);
         motionValues.put("True Charge Slash II (second hit)", 0.182);
         motionValues.put("True Charge Slash III (first hit)", 0.21);
-        motionValues.put("True Charge Slash III (second hit)", 0.210);
+        motionValues.put("True Charge Slash III (second hit)", 2.10);
         motionValues.put("True Charge Slash Overcharge (first hit)", 0.20);
         motionValues.put("True Charge Slash Overcharge (second hit)", 1.82);
         motionValues.put("Kick", .04);
@@ -53,12 +49,17 @@ public class GreatSword extends Blademaster {
         motionValues.put("Falling Thrust III ", 0.26);
     }
 
+    public GreatSword(String weaponName, int rarity, int attack, int affinity, int elderseal, String sharpnessColor, String element){
+        super(weaponName, rarity, attack, affinity, elderseal, sharpnessColor, element);
+
+    }
+
     protected void trueDamage(){
         this.trueDamage = this.getAttack()/4.8;
     }
     
 
-    public String getWeaponType(){
-        return weaponType;
+    public String getWEAPON_TYPE(){
+        return WEAPON_TYPE;
     }
 }
